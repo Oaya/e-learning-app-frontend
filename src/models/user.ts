442b239c351cd8f871d44f10dba89e3a5998a29b
Aya <ayaaa.okzk@gmail.com>
2026-06-11 22:31:54 -0700

    update Subscription Banner

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,10 +1,10 @@
-import type { User, AdminUser } from "../type/user";
+import type { User } from "../type/user";
 import { capitalize } from "../utils/helper";
 
 export class UserModel {
   private user: User | null | undefined;
 
-  constructor(user: User | AdminUser | null | undefined) {
+  constructor(user: User | null | undefined) {
     this.user = user;
   }
 
